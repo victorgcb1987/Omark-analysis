aacb21fa246e32c3f2ec9638cc1916fd798abbcc
Victor García-Carpintero Burgos <victor.gcb@gmail.com>
2026-04-28 12:00:55 +0200

    Update 02_digest_omark_gaqet_Bombarely_data.py

--- 02_digest_omark_gaqet_Bombarely_data.py
+++ 02_digest_omark_gaqet_Bombarely_data.py
@@ -14,7 +14,7 @@ OMARK_CLASS = ["Consistent_Full", "Consistent_Partial",
 
 IGNORE = ["Lupinus_albus", "Citrullus_lanatus", "Chelidonium_majus", 
           "Aegilops_comosa", "Mangifera_indica", "Gossypium_arboreum",
-          "Humulus_lupulus"]
+          "Humulus_lupulus", "Theobroma_cacao"]
 
 DETENGA_CLASS = ["PcpM0", "PteMte", "P0Mte", "PchMte", "PchM0", "PteM0", "PcpMte", "P0M0"]
 
